43d7bbd8c851c8272e38a11e8eabc967f4d1c8ce
greemoid <greemoiddd@gmail.com>
2022-06-13 17:52:11 +0300

    lesson-2(added screens)

---
+++ app/src/main/java/com/greemoid/notesappmvvm/navigation/NotesNavHost.kt
@@ -0,0 +1,29 @@
+package com.greemoid.notesappmvvm.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.greemoid.notesappmvvm.screens.AddScreen
+import com.greemoid.notesappmvvm.screens.MainScreen
+import com.greemoid.notesappmvvm.screens.NoteScreen
+import com.greemoid.notesappmvvm.screens.StartScreen
+
+sealed class NavRoute(val route: String) {
+    object Start: NavRoute("start_screen")
+    object Main: NavRoute("main_screen")
+    object Add: NavRoute("add_screen")
+    object Note: NavRoute("note_screen")
+}
+
+@Composable
+fun NotesNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavRoute.Start.route) {
+        composable(NavRoute.Start.route) { StartScreen(navController = navController) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController) }
+    }
+}
